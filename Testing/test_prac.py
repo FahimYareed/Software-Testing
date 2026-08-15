ae9d690f13508df350e42f63fff5eb7eff63ced9
Fahim Yareed <67155282+FahimYareed@users.noreply.github.com>
2026-08-15 21:37:38 +0600

    Refactor test_add and test_sub to simplify assertions
    Removed allure steps from test functions.

--- Testing/test_prac.py
+++ Testing/test_prac.py
@@ -1,13 +1,8 @@
-import allure
 
 a, b = 2, 3
 
-@allure.suite("Arithmetic Operations")
 def test_add():
-    with allure.step("Addition"):
-        assert a + b == 5
+    assert a + b == 5
 
-@allure.suite("Arithmetic Operations")
 def test_sub():
-    with allure.step("Subtraction"):
-        assert b - a == 1
+    assert b - a == 1
